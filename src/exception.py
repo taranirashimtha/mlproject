ec68d4058cdf4e5c6df471ff08d02d33bc7a56e8
taranirashmitha <taraniappari@gmail.com>
2024-09-17 18:18:42 +0530

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 # sys module provides various functions and variables used to manipulate diff python runtime envirornments
-import logging
+from src.logger import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
